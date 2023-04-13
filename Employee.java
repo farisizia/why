d79e6e2c3eb2e32f82609d0ec30764e534c8d8c7
farisizia <izziasroris@gmail.com>
2023-04-14 01:27:17 +0700

    cobaan

--- Employee.java
+++ Employee.java
@@ -7,7 +7,16 @@ import java.util.List;
 
 public class Employee {
 
+	private String employeeId;
+	private String firstName;
+	private String lastName;
+	private String idNumber;
+	private String address;
 	
+	private int yearJoined;
+	private int monthJoined;
+	private int dayJoined;
+	private int monthWorkingInYear;
 	
 	private boolean isForeigner;
 	private boolean gender; //true = Laki-laki, false = Perempuan
