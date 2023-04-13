46d3f87558cd71dffcb66c091def101b85f5bc87
farisizia <izziasroris@gmail.com>
2023-04-14 01:29:43 +0700

    statement

--- Employee.java
+++ Employee.java
@@ -15,12 +15,7 @@ public class Employee {
 	private int otherMonthlyIncome;
 	private int annualDeductible;
 	
-	private String spouseName;
-	private String spouseIdNumber;
 
-	private List<String> childNames;
-	private List<String> childIdNumbers;
-	
 	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
 		this.employeeId = employeeId;
 		this.firstName = firstName;
