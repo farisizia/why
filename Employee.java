d8477330a10420a7e53c048d87a2091e633fce1c
farisizia <izziasroris@gmail.com>
2023-04-14 15:41:49 +0700

    debi cantik

--- Employee.java
+++ Employee.java
@@ -1,20 +1,3 @@
-package lib;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.LinkedList;
-import java.util.List;
-
-public class Employee {
-
-
-	private boolean isForeigner;
-	private boolean gender; //true = Laki-laki, false = Perempuan
-	
-	private int monthlySalary;
-	private int otherMonthlyIncome;
-	private int annualDeductible;
-	
 
 	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
 		this.employeeId = employeeId;
